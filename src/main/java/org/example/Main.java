147737b089d6487028a9d7bc4134f4dae433d19d
1-21-01-E043-10\POO <paul.cabrera@epn.edu.ec>
2024-05-30 08:46:02 -0500

    Cambio 6

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,24 @@
 package org.example;
-
 import Procesadores.Procesadores;
+import Procesadores.Procesador_AMD;
+import Procesadores.Procesadores_moviles;
+import Procesadores.Procesador_Intel;
+
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Procesadores newProcesadores = new Procesadores();
-        ///Hola xd
-        //xdddd
-        //ojala
+        //Integrante 1
+        Procesador_AMD p1 = new Procesador_AMD();
+        Procesadores_moviles p2 = new Procesadores_moviles();
+        Procesador_Intel p3 = new Procesador_Intel();
+
+
+
+
+
 
     }
 }
