3ae099531fdad1874dbfe4edb5037485e57c2067
1-21-01-E043-09\POO <santiago.cumbalepn.edu.ec>
2024-05-30 08:00:01 -0500

    cambio escritorio

---
+++ src/main/java/Procesadores/Procesadores_escritorio.java
@@ -0,0 +1,8 @@
+package Procesadores;
+
+public class Procesadores_escritorio extends Procesadores {
+    String socket;
+    public Procesadores_escritorio() {}
+    public Procesadores_escritorio(int nucleos, String socket) {}
+
+}
