f2c611e89d7a222d8bfddd4e65e54175dff3f525
1-21-01-E043-10\POO <paul.cabrera@epn.edu.ec>
2024-05-30 07:53:10 -0500

    Cambio3

---
+++ src/main/java/procesadores/procesadores.java
@@ -0,0 +1,47 @@
+package procesadores;
+
+public class procesadores {
+    int nucleos;
+    Double consumo, velocidad;
+    String nombre;
+    public procesadores() {}
+
+    public procesadores(int nucleos, Double consumo, Double velocidad, String nombre) {
+        this.nucleos = nucleos;
+        this.consumo = consumo;
+        this.velocidad = velocidad;
+        this.nombre = nombre;
+    }
+
+    public int getNucleos() {
+        return nucleos;
+    }
+
+    public void setNucleos(int nucleos) {
+        this.nucleos = nucleos;
+    }
+
+    public Double getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(Double velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public Double getConsumo() {
+        return consumo;
+    }
+
+    public void setConsumo(Double consumo) {
+        this.consumo = consumo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
