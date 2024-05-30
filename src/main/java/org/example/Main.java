e39fa5a701007cc3f9e997bd8ccf254cdf06a645
1-21-01-E043-10\POO <paul.cabrera@epn.edu.ec>
2024-05-30 08:09:10 -0500

    l.j

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ import Procesadores.Procesadores;
 public class Main {
     public static void main(String[] args) {
         Procesadores newProcesadores = new Procesadores();
+        ///Hola
 
     }
 }
